130a53fb8a33ac664d40f71d0128ac8e49db55a6
pavel <pavel.urvantcev@gmail.com>
2023-12-27 23:29:18 +0100

    application prep

---
+++ src/main/java/se/soundgood/Main.java
@@ -0,0 +1,16 @@
+package se.soundgood;
+
+public class Main {
+    public static void main(String... args) {
+        System.out.println("sdfs");
+
+        var oldql = "select * " +
+                "from" +
+                " AAA";
+
+        var sql = """
+                select *
+                from AAA
+                              """;
+    }
+}
